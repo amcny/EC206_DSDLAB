76179c2145af2d06bc72fc3a7e1bdc4b3a0241e3
Chaitanya Pullagura <101804578+amcny@users.noreply.github.com>
2023-11-22 07:20:14 +0530

    and_2.v
    And gate

---
+++ and_2.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/05/2023 10:22:44 PM
+// Design Name: 
+// Module Name: and_2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_2(
+    input a,b,
+    output c
+    );
+ assign c=a&b;
+endmodule
